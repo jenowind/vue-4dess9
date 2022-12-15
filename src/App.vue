37785587f52a49df1940191590feb7df4698f3a7
jenowind <jenowind@etribe.co.kr>
2022-12-15 10:53:54 +0900

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
 </script>
 
 <style>
-#app1 {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
